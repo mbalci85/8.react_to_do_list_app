a008470b0f64b6f7f395c836e681ad7c9caa18d4
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-02-18 22:22:09 +0000

    added state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-	return <div className="App"></div>;
+export class App extends Component {
+	state = {
+		todos: [
+			{
+				id: 1,
+				task: 'Go',
+				isChecked: false,
+			},
+			{
+				id: 2,
+				task: 'Do',
+				isChecked: false,
+			},
+			{
+				id: 3,
+				task: 'Come',
+				isChecked: false,
+			},
+		],
+	};
+	render() {
+		return (
+			<div className="App">
+				<h1>TODO LIST</h1>
+			</div>
+		);
+	}
 }
 
 export default App;
