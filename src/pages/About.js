a091adb6d0723ec7ff6a6150efec6b36fc76bbd7
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-02-20 23:22:33 +0000

    added about page

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class About extends Component {
+	render() {
+		return (
+			<div>
+				<h4>About</h4>
+				<p>This is ToDoList App</p>
+			</div>
+		);
+	}
+}
+
+export default About;
